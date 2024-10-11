fefe7da1cbb38523b59efd0490e910b3cab1f6fc
Chris <chye0001@stud.kea.dk>
2024-10-11 14:29:14 +0200

    created GameStatServiceImpl and populated it with logic

---
+++ src/main/java/ccy/civilizationleaderboard/gamestat/GameStatServiceImpl.java
@@ -0,0 +1,72 @@
+package ccy.civilizationleaderboard.gamestat;
+
+import ccy.civilizationleaderboard.gamestat.dto.CreateGameStat;
+import ccy.civilizationleaderboard.gamestat.dto.EditGameStat;
+import ccy.civilizationleaderboard.gamestat.dto.GameStatResponse;
+import ccy.civilizationleaderboard.gamestat.mapper.CreateGameStatMapper;
+import ccy.civilizationleaderboard.gamestat.mapper.EditGameStatMapper;
+import ccy.civilizationleaderboard.gamestat.mapper.GameStatResponseMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class GameStatServiceImpl implements GameStatService {
+
+    private final GameStatResponseMapper gameStatResponseMapper;
+    private final CreateGameStatMapper createGameStatMapper;
+    private final EditGameStatMapper editGameStatMapper;
+    private final GameStatRepository gameStatRepository;
+
+
+
+
+    @Override
+    public GameStatResponse getGameStatById(int id) {
+
+        Optional<GameStat> gameStatOptional = gameStatRepository.findById(id);
+        if (gameStatOptional.isEmpty()) {
+            return null;
+        }
+
+        GameStat gameStat = gameStatOptional.get();
+        return gameStatResponseMapper.apply(gameStat);
+    }
+
+
+
+
+
+    @Override
+    public GameStatResponse createGameStat(CreateGameStat createRequest) {
+
+        GameStat gameStat = createGameStatMapper.apply(createRequest);
+        GameStat savedGameStat = gameStatRepository.save(gameStat);
+
+        return gameStatResponseMapper.apply(savedGameStat);
+    }
+
+
+
+
+
+    @Override
+    public GameStatResponse editGameStat(EditGameStat editRequest) {
+
+        GameStat gameStat = editGameStatMapper.apply(editRequest);
+        GameStat editedGameStat = gameStatRepository.save(gameStat);
+
+        return gameStatResponseMapper.apply(editedGameStat);
+    }
+
+
+
+
+
+    @Override
+    public void deleteGameStatById(int id) {
+        gameStatRepository.deleteById(id);
+    }
+}
