d5ab2a6187531e7be2c1b6795c653092dd79dda9
Chris <chye0001@stud.kea.dk>
2024-10-11 15:12:23 +0200

    created method to post new GameStat in GameStatController

--- src/main/java/ccy/civilizationleaderboard/gamestat/GameStatController.java
+++ src/main/java/ccy/civilizationleaderboard/gamestat/GameStatController.java
@@ -1,13 +1,11 @@
 package ccy.civilizationleaderboard.gamestat;
 
+import ccy.civilizationleaderboard.gamestat.dto.CreateGameStat;
 import ccy.civilizationleaderboard.gamestat.dto.GameStatResponse;
 import ccy.civilizationleaderboard.gamestat.service.GameStatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/gamestat")
@@ -19,13 +17,25 @@ public class GameStatController {
 
     @GetMapping("/{gameStatId}")
     public ResponseEntity<GameStatResponse> getGameStatById(@PathVariable int gameStatId) {
-
         GameStatResponse gameStatResponse = gameStatService.getGameStatById(gameStatId);
 
         if (gameStatResponse == null) {
             return ResponseEntity.notFound().build();
         }
 
+        return ResponseEntity.ok(gameStatResponse);
+    }
+
+
+    @PostMapping("")
+    public ResponseEntity<GameStatResponse> createGameStat(@RequestBody CreateGameStat createRequest) {
+
+        boolean doesExist = gameStatService.doesGameStatExist(createRequest);
+        if (!doesExist) {
+            return ResponseEntity.badRequest().build();
+        }
+
+        GameStatResponse gameStatResponse = gameStatService.createGameStat(createRequest);
         return ResponseEntity.ok(gameStatResponse);
     }
 }
