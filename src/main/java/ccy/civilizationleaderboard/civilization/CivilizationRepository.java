0823c76c8d575e0eb6ba0dc1bdc3fad596cedbe4
Chris <chye0001@stud.kea.dk>
2024-10-11 11:51:01 +0200

    created CivilizationRepository

---
+++ src/main/java/ccy/civilizationleaderboard/civilization/CivilizationRepository.java
@@ -0,0 +1,6 @@
+package ccy.civilizationleaderboard.civilization;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CivilizationRepository extends JpaRepository<Civilization, Integer> {
+}
