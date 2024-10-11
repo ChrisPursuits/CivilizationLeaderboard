a60f868344f1ac2d5f3f2add42375a0f29c74f3b
Chris <chye0001@stud.kea.dk>
2024-10-11 14:41:42 +0200

    created public ResponseEntity<GameStatResponse> getGameStatById(@PathVariable int gameStatId) in GameStatController

--- src/main/java/ccy/civilizationleaderboard/gamestat/GameStatController.java
+++ src/main/java/ccy/civilizationleaderboard/gamestat/GameStatController.java
@@ -1,16 +1,31 @@
 package ccy.civilizationleaderboard.gamestat;
 
+import ccy.civilizationleaderboard.gamestat.dto.GameStatResponse;
 import ccy.civilizationleaderboard.gamestat.service.GameStatService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1")
+@RequestMapping("/api/v1/gamestat")
 @RequiredArgsConstructor
 public class GameStatController {
 
     private final GameStatService gameStatService;
 
 
+    @GetMapping("/{gameStatId}")
+    public ResponseEntity<GameStatResponse> getGameStatById(@PathVariable int gameStatId) {
+
+        GameStatResponse gameStatResponse = gameStatService.getGameStatById(gameStatId);
+
+        if (gameStatResponse == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        return ResponseEntity.ok(gameStatResponse);
+    }
 }
