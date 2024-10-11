3e497192a4cae466091c119d2b0d41c42ff2d710
Chris <chye0001@stud.kea.dk>
2024-10-11 15:10:15 +0200

    added costume jpql method to check existence of gameStat on fields other than id.

--- src/main/java/ccy/civilizationleaderboard/gamestat/GameStatRepository.java
+++ src/main/java/ccy/civilizationleaderboard/gamestat/GameStatRepository.java
@@ -1,6 +1,7 @@
 package ccy.civilizationleaderboard.gamestat;
 
 import ccy.civilizationleaderboard.gamestat.model.GameStat;
+import ccy.civilizationleaderboard.gamestat.model.Placement;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Set;
@@ -8,4 +9,15 @@ import java.util.Set;
 public interface GameStatRepository extends JpaRepository<GameStat, Integer> {
 
     Set<GameStat> findGameStatsByGameId(int gameId);
+
+    //TODO improve this method using a combination of placement, militaryPoints etc. as a unique identifier in the db
+    boolean existsByPlacementAndVictoryPointsAndMilitaryPointsAndSciencePointsAndCulturePointsAndGoldAndReligiousPointsAndDiplomaticPoints(
+            Placement placement,
+            int victoryPoints,
+            int militaryPoints,
+            int sciencePoints,
+            int culturePoints,
+            int gold,
+            int religiousPoints,
+            int diplomaticPoints);
 }
