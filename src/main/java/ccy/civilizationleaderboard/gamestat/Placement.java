a1aa718f2a38d06e90195a6ce53a9c8dfd04989c
Chris <chye0001@stud.kea.dk>
2024-10-11 11:42:10 +0200

    created enum Placement containing 12 Placements

---
+++ src/main/java/ccy/civilizationleaderboard/gamestat/Placement.java
@@ -0,0 +1,17 @@
+package ccy.civilizationleaderboard.gamestat;
+
+public enum Placement {
+    FIRST,
+    SECOND,
+    THIRD,
+    FOURTH,
+    FIFTH,
+    SIXTH,
+    SEVENTH,
+    EIGHTH,
+    NINTH,
+    TENTH,
+    ELEVEN,
+    TWELFTH,
+
+}
