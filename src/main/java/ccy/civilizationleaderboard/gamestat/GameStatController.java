6893b0defb0bda15a759f8d990a55249411e56fe
Chris <chye0001@stud.kea.dk>
2024-10-11 14:29:26 +0200

    created GameStatController

---
+++ src/main/java/ccy/civilizationleaderboard/gamestat/GameStatController.java
@@ -0,0 +1,15 @@
+package ccy.civilizationleaderboard.gamestat;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+@RequiredArgsConstructor
+public class GameStatController {
+
+    private final GameStatService gameStatService;
+
+
+}
