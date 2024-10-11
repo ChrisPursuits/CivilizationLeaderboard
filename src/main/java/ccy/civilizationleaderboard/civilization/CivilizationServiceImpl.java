27f5d2d0ec40bb137d5a576c11c7dbb279e2be89
Chris <chye0001@stud.kea.dk>
2024-10-11 11:51:30 +0200

    created CivilizationServiceImpl and implemented CivilizationService

---
+++ src/main/java/ccy/civilizationleaderboard/civilization/CivilizationServiceImpl.java
@@ -0,0 +1,17 @@
+package ccy.civilizationleaderboard.civilization;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CivilizationServiceImpl implements CivilizationService {
+
+    @Override
+    public Civilization getCivilizationBy(int id) {
+        return null;
+    }
+
+    @Override
+    public Civilization getCivilizationBy(String name) {
+        return null;
+    }
+}
