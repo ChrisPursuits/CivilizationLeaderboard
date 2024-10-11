cf6b9eb99c823501b02c45dae3155462e77ee6cc
Chris <chye0001@stud.kea.dk>
2024-10-11 08:39:41 +0200

    initial commit

---
+++ src/main/java/ccy/civilizationleaderboard/CivilizationLeaderboardApplication.java
@@ -0,0 +1,13 @@
+package ccy.civilizationleaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CivilizationLeaderboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CivilizationLeaderboardApplication.class, args);
+    }
+
+}
