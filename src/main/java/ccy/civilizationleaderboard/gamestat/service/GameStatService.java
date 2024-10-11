e88a7499573200356bbcfffa89c6fb926b9e4f0f
Chris <chye0001@stud.kea.dk>
2024-10-11 14:35:21 +0200

    added new method: Set<GameStatResponse> getAllGameStatsByGameId(int gameId); to GameStatService

--- src/main/java/ccy/civilizationleaderboard/gamestat/service/GameStatService.java
+++ src/main/java/ccy/civilizationleaderboard/gamestat/service/GameStatService.java
@@ -4,8 +4,11 @@ import ccy.civilizationleaderboard.gamestat.dto.CreateGameStat;
 import ccy.civilizationleaderboard.gamestat.dto.EditGameStat;
 import ccy.civilizationleaderboard.gamestat.dto.GameStatResponse;
 
+import java.util.Set;
+
 public interface GameStatService {
     GameStatResponse getGameStatById(int id );
+    Set<GameStatResponse> getAllGameStatsByGameId(int gameId);
     GameStatResponse createGameStat(CreateGameStat gameStat);
     GameStatResponse editGameStat(EditGameStat gameStat);
     void deleteGameStatById(int id);
