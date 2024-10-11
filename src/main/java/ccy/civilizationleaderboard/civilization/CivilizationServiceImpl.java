95b381c777298f779283ab70e8aecfa99e907f02
Chris <chye0001@stud.kea.dk>
2024-10-11 12:00:34 +0200

    - populated methods in CivilizationServiceImpl with logic

--- src/main/java/ccy/civilizationleaderboard/civilization/CivilizationServiceImpl.java
+++ src/main/java/ccy/civilizationleaderboard/civilization/CivilizationServiceImpl.java
@@ -1,17 +1,40 @@
 package ccy.civilizationleaderboard.civilization;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
+@RequiredArgsConstructor
 public class CivilizationServiceImpl implements CivilizationService {
+
+    private final CivilizationRepository civilizationRepository;
+    private final RequestCivilizationMapper requestCivilizationMapper;
 
     @Override
     public RequestCivilization getCivilizationBy(int id) {
-        return null;
+        Optional<Civilization> civilizationOptional = civilizationRepository.findById(id);
+
+        if (civilizationOptional.isEmpty()) {
+            return null;
+        }
+
+        Civilization civilization = civilizationOptional.get();
+        RequestCivilization requestCivilization = requestCivilizationMapper.apply(civilization);
+        return requestCivilization;
     }
 
     @Override
     public RequestCivilization getCivilizationBy(String name) {
-        return null;
+        Optional<Civilization> civilizationOptional = civilizationRepository.findByCivilizationName(name);
+
+        if (civilizationOptional.isEmpty()) {
+            return null;
+        }
+
+        Civilization civilization = civilizationOptional.get();
+        RequestCivilization requestCivilization = requestCivilizationMapper.apply(civilization);
+        return requestCivilization;
     }
 }
