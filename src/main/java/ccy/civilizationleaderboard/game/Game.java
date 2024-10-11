987a73561ca6793803cc2b6d0232f1ff9b21870e
Chris <chye0001@stud.kea.dk>
2024-10-11 11:43:44 +0200

    created model: Game and connected it to User

---
+++ src/main/java/ccy/civilizationleaderboard/game/Game.java
@@ -0,0 +1,33 @@
+package ccy.civilizationleaderboard.game;
+
+import ccy.civilizationleaderboard.gamestat.GameStat;
+import ccy.civilizationleaderboard.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Game {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String title;
+    private String description;
+
+    @OneToOne(mappedBy = "game")
+    private User user;
+
+    @OneToMany
+    @JoinColumn(name = "game_stat_id")
+    private Set<GameStat> gameStat;
+}
