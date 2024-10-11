5885858e32dbe76aba02f5db2d0e1d34ac0d5702
Chris <chye0001@stud.kea.dk>
2024-10-11 14:34:17 +0200

    added new costume Jpql method: Set<GameStat> findGameStatsByGameId(int gameId);

--- src/main/java/ccy/civilizationleaderboard/gamestat/GameStatRepository.java
+++ src/main/java/ccy/civilizationleaderboard/gamestat/GameStatRepository.java
@@ -3,5 +3,9 @@ package ccy.civilizationleaderboard.gamestat;
 import ccy.civilizationleaderboard.gamestat.model.GameStat;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Set;
+
 public interface GameStatRepository extends JpaRepository<GameStat, Integer> {
+
+    Set<GameStat> findGameStatsByGameId(int gameId);
 }
