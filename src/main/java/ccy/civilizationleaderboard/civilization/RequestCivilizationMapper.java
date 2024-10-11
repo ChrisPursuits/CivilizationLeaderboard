9071e5a053b90d23970ec7f3f4ccbfec9ab48a9d
Chris <chye0001@stud.kea.dk>
2024-10-11 12:00:07 +0200

    annotated RequestCivilizationMapper with @Component

--- src/main/java/ccy/civilizationleaderboard/civilization/RequestCivilizationMapper.java
+++ src/main/java/ccy/civilizationleaderboard/civilization/RequestCivilizationMapper.java
@@ -1,8 +1,12 @@
 package ccy.civilizationleaderboard.civilization;
 
+import org.springframework.stereotype.Component;
+
 import java.util.function.Function;
 
+@Component
 public class RequestCivilizationMapper implements Function<Civilization, RequestCivilization> {
+
     @Override
     public RequestCivilization apply(Civilization civilization) {
         return new RequestCivilization(
