347dae3f0e1b976a79c78be0c2590896088401b3
Chris <chye0001@stud.kea.dk>
2024-10-11 10:55:46 +0200

    created model: Leaderboard

---
+++ src/main/java/ccy/civilizationleaderboard/leaderboard/Leaderboard.java
@@ -0,0 +1,27 @@
+package ccy.civilizationleaderboard.leaderboard;
+
+import ccy.civilizationleaderboard.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Set;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Leaderboard {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String description;
+
+    @OneToMany(mappedBy = "leaderboard")
+    private Set<User> user;
+}
