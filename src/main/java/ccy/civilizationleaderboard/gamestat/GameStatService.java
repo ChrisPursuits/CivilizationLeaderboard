8399264789d1d897bed398570de8219a3f25fa2a
Chris <chye0001@stud.kea.dk>
2024-10-11 14:06:28 +0200

    created GameStatService

--- src/main/java/ccy/civilizationleaderboard/gamestat/GameStatService.java
+++ src/main/java/ccy/civilizationleaderboard/gamestat/GameStatService.java
@@ -1,12 +1,13 @@
 package ccy.civilizationleaderboard.gamestat;
 
 import ccy.civilizationleaderboard.gamestat.dto.CreateGameStat;
+import ccy.civilizationleaderboard.gamestat.dto.EditGameStat;
 import ccy.civilizationleaderboard.gamestat.dto.RequestGameStat;
 
 public interface GameStatService {
     RequestGameStat getGameStatById(int id );
     RequestGameStat createGameStat(CreateGameStat gameStat);
-    RequestGameStat editGameStat();
+    RequestGameStat editGameStat(EditGameStat gameStat);
     void deleteGameStatById(int id);
 
 }
