4b971a3ef016946b728fa0606b642115e8479d18
Chris <chye0001@stud.kea.dk>
2024-10-11 11:59:42 +0200

    added costume jpa method: Optional<Civilization> findByCivilizationName(String name)

--- src/main/java/ccy/civilizationleaderboard/civilization/CivilizationRepository.java
+++ src/main/java/ccy/civilizationleaderboard/civilization/CivilizationRepository.java
@@ -2,5 +2,9 @@ package ccy.civilizationleaderboard.civilization;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CivilizationRepository extends JpaRepository<Civilization, Integer> {
+
+    Optional<Civilization> findByCivilizationName(String civilizationName);
 }
