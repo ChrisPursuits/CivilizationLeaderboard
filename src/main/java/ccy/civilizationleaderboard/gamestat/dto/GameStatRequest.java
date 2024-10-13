32d2922055c8c43e1a755218a2bcf640708e15c1
Chris <chye0001@stud.kea.dk>
2024-10-13 22:28:55 +0200

    added attribute int id to dto: GameStatRequest

--- src/main/java/ccy/civilizationleaderboard/gamestat/dto/GameStatRequest.java
+++ src/main/java/ccy/civilizationleaderboard/gamestat/dto/GameStatRequest.java
@@ -3,6 +3,7 @@ package ccy.civilizationleaderboard.gamestat.dto;
 import ccy.civilizationleaderboard.gamestat.model.Placement;
 
 public record GameStatRequest(
+        int id,
         Placement placement,
         int victoryPoints,
         int militaryPoints,
