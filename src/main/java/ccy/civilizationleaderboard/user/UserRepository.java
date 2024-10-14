ae5cff66b4b4bc8f3bb59271b9182e363db67968
Chris <chye0001@stud.kea.dk>
2024-10-14 10:59:05 +0200

    created UserRepository and added costume JPQL method; User findByUsername(String username);

---
+++ src/main/java/ccy/civilizationleaderboard/user/UserRepository.java
@@ -0,0 +1,7 @@
+package ccy.civilizationleaderboard.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+}
