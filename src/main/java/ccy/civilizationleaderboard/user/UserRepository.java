82980194484e84455344c3c920dd40adca6050d8
Chris <chye0001@stud.kea.dk>
2024-10-15 09:45:48 +0200

    added jpql methods: - List<User> findAllByLeaderboardId(int leaderboardId); - List<GameStat> findAllGameStatsByUsername(String username);

--- src/main/java/ccy/civilizationleaderboard/user/UserRepository.java
+++ src/main/java/ccy/civilizationleaderboard/user/UserRepository.java
@@ -1,9 +1,21 @@
 package ccy.civilizationleaderboard.user;
 
+import ccy.civilizationleaderboard.gamestat.model.GameStat;
+import ccy.civilizationleaderboard.user.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
+    List<User> findAllByLeaderboardId(int leaderboardId);
+
+    @Query("SELECT gs FROM GameStat gs " +
+            "JOIN gs.game g " +
+            "JOIN g.user u " +
+            "WHERE u.username = :username")
+    List<GameStat> findAllGameStatsByUsername(String username);
+
 }
