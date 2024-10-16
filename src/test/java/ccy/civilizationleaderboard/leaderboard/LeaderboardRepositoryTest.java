3fe424b1e78bc49d163ebdfd13db4afca78a92ba
Chris <chye0001@stud.kea.dk>
2024-10-16 10:06:24 +0200

    created LeaderboardRepositoryTest

---
+++ src/test/java/ccy/civilizationleaderboard/leaderboard/LeaderboardRepositoryTest.java
@@ -0,0 +1,31 @@
+package ccy.civilizationleaderboard.leaderboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.jdbc.Sql;
+
+import static org.junit.jupiter.api.Assertions.*;
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:init_db.sql")
+@ActiveProfiles("test")
+@DataJpaTest
+class LeaderboardRepositoryTest {
+
+    @Autowired
+    private LeaderboardRepository leaderboardRepository;
+
+    @Test
+    void existsByNameAndDescription_ReturnTrue() {
+        Leaderboard leaderboard = new Leaderboard("First Leaderboard", "This is a test leaderboard");
+
+        assertTrue(leaderboardRepository.existsByNameAndDescription(leaderboard.getName(), leaderboard.getDescription()));
+    }
+
+    @Test
+    void existsByNameAndDescription_ReturnFalse() {
+        Leaderboard leaderboard = new Leaderboard("This does not exist", "Neither does this");
+
+        assertFalse(leaderboardRepository.existsByNameAndDescription(leaderboard.getName(), leaderboard.getDescription()));
+    }
+}
