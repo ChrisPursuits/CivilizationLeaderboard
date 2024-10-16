99e8b0d6a69e276552e0a7115e164d3188a0e33d
Chris <chye0001@stud.kea.dk>
2024-10-16 12:33:42 +0200

    added User and civilization as attributes to GameStatRequest

--- src/main/java/ccy/civilizationleaderboard/gamestat/dto/GameStatRequest.java
+++ src/main/java/ccy/civilizationleaderboard/gamestat/dto/GameStatRequest.java
@@ -1,8 +1,12 @@
 package ccy.civilizationleaderboard.gamestat.dto;
 
+import ccy.civilizationleaderboard.civilization.model.Civilization;
 import ccy.civilizationleaderboard.gamestat.model.Placement;
+import ccy.civilizationleaderboard.user.model.User;
 
 public record GameStatRequest(
+        User user,
+        Civilization selectedCivilization,
         Placement placement,
         int victoryPoints,
         int militaryPoints,
