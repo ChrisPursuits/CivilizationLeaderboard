09c15b361f11e3939f33ac594c713bd37e58f343
Chris <chye0001@stud.kea.dk>
2024-10-16 12:34:01 +0200

    made use of updated GameStat constructor

--- src/main/java/ccy/civilizationleaderboard/gamestat/mapper/GameStatRequestMapper.java
+++ src/main/java/ccy/civilizationleaderboard/gamestat/mapper/GameStatRequestMapper.java
@@ -2,17 +2,22 @@ package ccy.civilizationleaderboard.gamestat.mapper;
 
 import ccy.civilizationleaderboard.gamestat.model.GameStat;
 import ccy.civilizationleaderboard.gamestat.dto.GameStatRequest;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.function.Function;
 
 @Component
+@RequiredArgsConstructor
 public class GameStatRequestMapper implements Function<GameStatRequest, GameStat> {
-
 
     @Override
     public GameStat apply(GameStatRequest gameStatRequest) {
+
+
         return new GameStat(
+                gameStatRequest.user(),
+                gameStatRequest.selectedCivilization(),
                 gameStatRequest.placement(),
                 gameStatRequest.victoryPoints(),
                 gameStatRequest.militaryPoints(),
