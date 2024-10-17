53afa8a6c6d7718bbb7c2f9d68eb487e3823b75d
Chris <chye0001@stud.kea.dk>
2024-10-17 10:21:57 +0200

    refactored test classes

--- src/test/java/ccy/civilizationleaderboard/repository/LeaderboardRepositoryTest.java
+++ src/test/java/ccy/civilizationleaderboard/repository/LeaderboardRepositoryTest.java
@@ -4,14 +4,17 @@ import ccy.civilizationleaderboard.leaderboard.Leaderboard;
 import ccy.civilizationleaderboard.leaderboard.LeaderboardRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@ActiveProfiles("test")
-@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:init_db.sql")
+//@ActiveProfiles("test")
+//@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:init_db.sql")
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 @DataJpaTest
 class LeaderboardRepositoryTest {
 
@@ -19,14 +22,16 @@ class LeaderboardRepositoryTest {
     private LeaderboardRepository leaderboardRepository;
 
     @Test
-    void existsByNameAndDescription_ReturnTrue() {
+    void LeaderboardRepository_existsByNameAndDescription_ReturnTrue() {
         Leaderboard leaderboard = new Leaderboard("First Leaderboard", "This is a test leaderboard");
+
+        leaderboardRepository.save(leaderboard);
 
         assertTrue(leaderboardRepository.existsByNameAndDescription(leaderboard.getName(), leaderboard.getDescription()));
     }
 
     @Test
-    void existsByNameAndDescription_ReturnFalse() {
+    void LeaderboardRepository_existsByNameAndDescription_ReturnFalse() {
         Leaderboard leaderboard = new Leaderboard("This does not exist", "Neither does this");
 
         assertFalse(leaderboardRepository.existsByNameAndDescription(leaderboard.getName(), leaderboard.getDescription()));
--- src/test/java/ccy/civilizationleaderboard/repository/UserRepositoryTest.java
+++ src/test/java/ccy/civilizationleaderboard/repository/UserRepositoryTest.java
@@ -1,30 +1,99 @@
 package ccy.civilizationleaderboard.repository;
 
+import ccy.civilizationleaderboard.leaderboard.Leaderboard;
+import ccy.civilizationleaderboard.leaderboard.LeaderboardRepository;
 import ccy.civilizationleaderboard.user.UserRepository;
+import ccy.civilizationleaderboard.user.model.Role;
 import ccy.civilizationleaderboard.user.model.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.jdbc.EmbeddedDatabaseConnection;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
-@ActiveProfiles("test")
-@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:init_db.sql")
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+//@ActiveProfiles("test")
+//@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:init_db.sql")
+
 @DataJpaTest
+@AutoConfigureTestDatabase(connection = EmbeddedDatabaseConnection.H2)
 class UserRepositoryTest {
 
     @Autowired
     private UserRepository userRepository;
 
+    @Autowired
+    private LeaderboardRepository leaderboardRepository;
+
     @Test
-    void findByUsername() {
+    void UserRepository_findByUsername_ReturnsUser() {
+        User user = User.builder()
+                .id(1)
+                .username("Chris")
+                .password("{encoded_password_for_Chris}")
+                .role(Role.ROLE_USER)
+                .build();
+
+        User expectedUser = userRepository.save(user);
+
+        User actualUser = userRepository.findByUsername("Chris").get();
+
+        assertEquals(expectedUser, actualUser);
     }
 
     @Test
-    void findAllUsersByLeaderboardIdSortedByPlacements() {
-    }
+    void UserRepository_findAllByLeaderboardId_ReturnsListOfLeaderboardMembers() {
 
-    @Test
-    void findAllGameStatsByUsername() {
+        Leaderboard leaderboard = new Leaderboard(1, "Leaderboard", "description", new ArrayList<>());
+        leaderboardRepository.save(leaderboard);
+
+        User chris = User.builder()
+                .id(1)
+                .username("Chris")
+                .password("{encoded_password_for_Chris}")
+                .role(Role.ROLE_USER)
+                .build();
+
+        User markus = User.builder()
+                .id(2)
+                .username("Markus")
+                .password("{encoded_password_for_Markus}")
+                .role(Role.ROLE_USER)
+                .build();
+
+        User engjëll = User.builder()
+                .id(3)
+                .username("Engjëll")
+                .password("{encoded_password_for_Engjëll}")
+                .role(Role.ROLE_USER)
+                .build();
+
+        User mikkel = User.builder()
+                .id(4)
+                .username("Mikkel")
+                .password("{encoded_password_for_Mikkel}")
+                .role(Role.ROLE_USER)
+                .build();
+
+
+        chris.setLeaderboardList(List.of(leaderboardRepository.findById(1).get())); markus.setLeaderboardList(List.of(leaderboardRepository.findById(1).get())); engjëll.setLeaderboardList(List.of(leaderboardRepository.findById(1).get())); mikkel.setLeaderboardList(List.of(leaderboardRepository.findById(1).get()));
+        userRepository.save(chris); userRepository.save(markus); userRepository.save(engjëll); userRepository.save(mikkel);
+
+        List<User> users = new ArrayList<>(List.of(userRepository.findByUsername("Chris").get(), userRepository.findByUsername("Markus").get(), userRepository.findByUsername("Engjëll").get(), userRepository.findByUsername("Mikkel").get()));
+        leaderboard.setLeaderboardMembers(users);
+        leaderboardRepository.save(leaderboard);
+
+        List<User> expectedUsers = new ArrayList<>(List.of(userRepository.findByUsername("Chris").get(), userRepository.findByUsername("Markus").get(), userRepository.findByUsername("Engjëll").get(), userRepository.findByUsername("Mikkel").get()));
+
+        int leaderboardId = 1;
+        List<User> actualUsers = userRepository.findAllByLeaderboardId(leaderboardId);
+
+        assertEquals(expectedUsers, actualUsers);
     }
 }
