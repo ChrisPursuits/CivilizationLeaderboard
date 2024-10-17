8fad436b4e1da5c314183871643a74b64638c2a6
Chris <chye0001@stud.kea.dk>
2024-10-17 10:17:57 +0200

    changed annotation @Data to @Getter and @Setter

--- src/main/java/ccy/civilizationleaderboard/leaderboard/Leaderboard.java
+++ src/main/java/ccy/civilizationleaderboard/leaderboard/Leaderboard.java
@@ -2,15 +2,13 @@ package ccy.civilizationleaderboard.leaderboard;
 
 import ccy.civilizationleaderboard.user.model.User;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Builder
 @NoArgsConstructor
