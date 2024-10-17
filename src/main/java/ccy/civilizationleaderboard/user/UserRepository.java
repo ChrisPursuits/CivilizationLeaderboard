beb616dbe91ad55d84a552f61fb05147def5a366
Chris <chye0001@stud.kea.dk>
2024-10-17 10:18:58 +0200

    removed old method that gets all users sorted by placement history and replaced it with a simpler: findAllByLeaderboardId

--- src/main/java/ccy/civilizationleaderboard/user/UserRepository.java
+++ src/main/java/ccy/civilizationleaderboard/user/UserRepository.java
@@ -11,19 +11,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUsername(String username);
 
-    @Query("SELECT u FROM User u JOIN u.leaderboardList l WHERE l.id = :leaderboardId " +
-            "ORDER BY " +
-            "(SELECT COUNT(ph) FROM u.placementHistory ph WHERE ph = 1) DESC, " +
-            "(SELECT COUNT(ph) FROM u.placementHistory ph WHERE ph = 2) DESC, " +
-            "(SELECT COUNT(ph) FROM u.placementHistory ph WHERE ph = 3) DESC, " +
-            "(SELECT COUNT(ph) FROM u.placementHistory ph WHERE ph = 4) DESC, " +
-            "(SELECT COUNT(ph) FROM u.placementHistory ph WHERE ph = 5) DESC, " +
-            "(SELECT COUNT(ph) FROM u.placementHistory ph WHERE ph = 6) DESC, " +
-            "(SELECT COUNT(ph) FROM u.placementHistory ph WHERE ph = 7) DESC, " +
-            "(SELECT COUNT(ph) FROM u.placementHistory ph WHERE ph = 8) DESC, " +
-            "(SELECT COUNT(ph) FROM u.placementHistory ph WHERE ph = 9) DESC, " +
-            "(SELECT COUNT(ph) FROM u.placementHistory ph WHERE ph = 10) DESC, " +
-            "(SELECT COUNT(ph) FROM u.placementHistory ph WHERE ph = 11) DESC, " +
-            "(SELECT COUNT(ph) FROM u.placementHistory ph WHERE ph = 12) DESC")
-    List<User> findAllUsersByLeaderboardIdSortedByPlacements(int leaderboardId);
+    @Query("SELECT u FROM User u JOIN u.leaderboardList l WHERE l.id = :leaderboardId")
+    List<User> findAllByLeaderboardId(int leaderboardId);
 }
