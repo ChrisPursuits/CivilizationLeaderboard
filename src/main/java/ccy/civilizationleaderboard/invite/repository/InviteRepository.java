536f874f3a1b17ea3769a00b4dea7340d7c0f84c
Chris <chye0001@stud.kea.dk>
2024-10-18 10:12:00 +0200

    created InviteRepository and added 1 costume JPQL method

---
+++ src/main/java/ccy/civilizationleaderboard/invite/repository/InviteRepository.java
@@ -0,0 +1,11 @@
+package ccy.civilizationleaderboard.invite.repository;
+
+import ccy.civilizationleaderboard.invite.model.Invite;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface InviteRepository extends JpaRepository<Invite, Integer> {
+
+    List<Invite> findAllByReceiverId(Integer receiverId);
+}
