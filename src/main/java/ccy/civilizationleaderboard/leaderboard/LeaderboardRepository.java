489af04e42be531621e0b52528d50682ad1985c6
Chris <chye0001@stud.kea.dk>
2024-10-19 17:25:03 +0200

    added new method for retriving all leaderboards by username to LeaderboardRepository

--- src/main/java/ccy/civilizationleaderboard/leaderboard/LeaderboardRepository.java
+++ src/main/java/ccy/civilizationleaderboard/leaderboard/LeaderboardRepository.java
@@ -1,8 +1,15 @@
 package ccy.civilizationleaderboard.leaderboard;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface LeaderboardRepository extends JpaRepository<Leaderboard, Integer> {
 
     boolean existsByNameAndDescription(String name, String description);
+
+    @Query("SELECT l FROM Leaderboard l JOIN l.leaderboardMembers u WHERE u.username = :username")
+    List<Leaderboard> findAllByUsername(String username);
+
 }
